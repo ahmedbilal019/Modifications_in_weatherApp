75858134bb4b47b8eac82f1c9c406c14fee31b86
ahmedbilal <ahmed258bilal@gmail.com>
2024-09-30 11:55:09 +0500

    deploying project and a little bit change in card size

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import '../App.css';
-import cloudy from '../cloudy.png';
 import axios from 'axios';
 import searchIcon from '../searchIcon.svg';
 
