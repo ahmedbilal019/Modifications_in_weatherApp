76c7a5454ec512dba42ce746128cceda448bc096
ahmedbilal <ahmed258bilal@gmail.com>
2024-09-30 15:03:41 +0500

    refactor

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-
-import './App.css';
-import Card from './components/Card';
+import "./App.css";
+import Card from "./components/Card";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
-      <Card/>
+      <header className="App-header"></header>
+      <Card />
     </div>
   );
 }
